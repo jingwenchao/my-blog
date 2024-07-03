fa2864e89bf1c62a2bbe3bc7a5e83ab7dc3d43bf
jingwenchao <jingwenchao@ruis.cn>
2024-07-03 17:17:26 +0800

    fix: 模型优化

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,40 +1,27 @@
 import { h, watch } from 'vue'
 import { useData, EnhanceAppContext } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
-
 import { createMediumZoomProvider } from './composables/useMediumZoom'
-
 import MLayout from './components/MLayout.vue'
 import MNavLinks from './components/MNavLinks.vue'
-
 import './styles/index.scss'
-
 let homePageStyle: HTMLStyleElement | undefined
-
 export default {
   extends: DefaultTheme,
   Layout: () => {
     const props: Record<string, any> = {}
     // 获取 frontmatter
     const { frontmatter } = useData()
-
     /* 添加自定义 class */
     if (frontmatter.value?.layoutClass) {
       props.class = frontmatter.value.layoutClass
     }
-
     return h(MLayout, props)
   },
   async enhanceApp({ app, router }: EnhanceAppContext) {
     createMediumZoomProvider(app, router)
-
-
     app.provide('DEV', process.env.NODE_ENV === 'development')
-
-
     app.component('MNavLinks', MNavLinks)
-
-
     if (typeof window !== 'undefined') {
       watch(
         () => router.route.data.relativePath,
@@ -46,8 +33,6 @@ export default {
         { immediate: true },
       )
     }
-
-
     if (!import.meta.env.SSR) {
       const { loadOml2d } = await import('oh-my-live2d')
       loadOml2d({
@@ -74,15 +59,15 @@ export default {
         statusBar: {},
         menus: {
           items: [
-            // {
-            //   id: 'Rest',
-            //   icon: 'icon-rest',
-            //   title: '休息',
-            //   onClick(oml2d): void {
-            //     oml2d.statusBarOpen('休息一会儿')
-            //     oml2d.stageSlideOut()
-            //   },
-            // },
+            {
+              id: 'Rest',
+              icon: 'icon-rest',
+              title: '休息',
+              onClick(oml2d): void {
+                oml2d.statusBarOpen('休息一会儿')
+                oml2d.stageSlideOut()
+              },
+            },
             {
               id: 'SwitchModel',
               icon: 'icon-switch',
@@ -92,6 +77,14 @@ export default {
                 oml2d.loadNextModel()
               },
             },
+            {
+              id: 'About',
+              icon: 'icon-about', 
+              title: '关于',
+              onClick(oml2d): void {
+                oml2d.loadNextModel();
+              }
+            }
           ],
         },
         models: [
@@ -108,35 +101,45 @@ export default {
           {
             path: 'https://model.oml2d.com/HK416-2-normal/model.json',
             // scale: 0.15,
-            position: [-60, -20],
-            stageStyle: {
-              width: 250,
-              height: 450,
-            },
+            position: [60, 30],
+            // stageStyle: {
+            //   width: 220,
+            //   height: 450,
+            // },
           },
-          {
-            path: 'https://model.oml2d.com/HK416-1-normal/model.json',
-            // scale: 0.15,
-            position: [-80, 0],
-            stageStyle: {
-              width: 350,
-              height: 480,
-            },
-          },
-          {
-            path: 'https://model.oml2d.com/Senko_Normals/senko.model3.json',
-            // scale: 0.15,
-            position: [-60, 160],
-            stageStyle: {
-              width: 250,
-              height: 450,
-            },
-          },
+          // {
+          //   path: 'https://model.oml2d.com/HK416-1-normal/model.json',
+          //   // scale: 0.15,
+          //   position: [-80, 0],
+          //   stageStyle: {
+          //     width: 350,
+          //     height: 480,
+          //   },
+          // },
+          // {
+          //   path: 'https://model.oml2d.com/Senko_Normals/senko.model3.json',
+          //   // scale: 0.15,
+          //   position: [-60, 160],
+          //   stageStyle: {
+          //     width: 250,
+          //     height: 450,
+          //   },
+          // },
         ],
       })
     }
   },
 }
+
+setTimeout(() => {
+  const oml2dStage = document.querySelectorAll('canvas')
+  // if (oml2dStage) {
+  //   oml2dStage.style.display = 'block'
+  // }
+  oml2dStage.forEach((oml2dStage) => {
+    makeElementDraggable(oml2dStage)
+  })
+}, 1000)
 
 if (typeof window !== 'undefined') {
   // detect browser, add to class for conditional styling
@@ -154,7 +157,6 @@ if (typeof window !== 'undefined') {
 function updateHomePageStyle(value: boolean) {
   if (value) {
     if (homePageStyle) return
-
     homePageStyle = document.createElement('style')
     homePageStyle.innerHTML = `
     :root {
@@ -163,8 +165,29 @@ function updateHomePageStyle(value: boolean) {
     document.body.appendChild(homePageStyle)
   } else {
     if (!homePageStyle) return
-
     homePageStyle.remove()
     homePageStyle = undefined
   }
+}
+
+// 函数：使元素可以拖动
+function makeElementDraggable(element: HTMLElement) {
+  let offsetX = 0, offsetY = 0, initialX = 0, initialY = 0;
+  const onMouseDown = (event: MouseEvent) => {
+    event.preventDefault();
+    initialX = event.clientX - offsetX;
+    initialY = event.clientY - offsetY;
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  };
+  const onMouseMove = (event: MouseEvent) => {
+    offsetX = event.clientX - initialX;
+    offsetY = event.clientY - initialY;
+    element.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
+  };
+  const onMouseUp = () => {
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+  };
+  element.addEventListener('mousedown', onMouseDown);
 }
